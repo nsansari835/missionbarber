fc2f9168439418cc0533bb23e081c86ab379632d
Yonus A <nsansari835@gmail.com>
2019-08-25 03:18:33 -0700

    Change directory name

--- server.js
+++ server.js
@@ -22,82 +22,82 @@ app.get('/',function(req,res) {
   });
 
 
-app.get('/store', function(req, res) {
-    fs.readFile('items.json', function(error, data){
-        if(error) {
-            res.status(500).end()
-        } else {
-            res.render('store.ejs', {
-                stripePublicKey: stripePublicKey,
-                items: JSON.parse(data)
-            })
-        }
-    })
-})
+// app.get('/store', function(req, res) {
+//     fs.readFile('items.json', function(error, data){
+//         if(error) {
+//             res.status(500).end()
+//         } else {
+//             res.render('store.ejs', {
+//                 stripePublicKey: stripePublicKey,
+//                 items: JSON.parse(data)
+//             })
+//         }
+//     })
+// })
 
-app.post('/appointment', function(req, res) {
-    fs.readFile('items.json', function(error, data){
-        if(error) {
-            res.status(500).end()
-        } else {
-            console.log('purchase', data)
-            const itemsJson = JSON.parse(data)
-            const itemsArray = itemsJson.music.concat(itemsJson.merch)
-            let total = 0
-            req.body.items.forEach(function(item) {
-                const itemJson = itemsArray.find(function(i){
-                    return i.id == item.id
-                })
-                total = total + itemJson.price * item.quantity
-                console.log(total)
-            })
-            stripe.charges.create({
-                amount: total,
-                source: req.body.stripeTokenId,
-                currency: 'usd'
-            }).then(function(){
-                console.log('Charge Successful')
-                res.json({ message: 'Successfully purchased items'})
-            }).catch(function(){
-                console.log('Charge Fail')
-                res.status(500).end()
-            })
+// app.post('/appointment', function(req, res) {
+//     fs.readFile('items.json', function(error, data){
+//         if(error) {
+//             res.status(500).end()
+//         } else {
+//             console.log('purchase', data)
+//             const itemsJson = JSON.parse(data)
+//             const itemsArray = itemsJson.music.concat(itemsJson.merch)
+//             let total = 0
+//             req.body.items.forEach(function(item) {
+//                 const itemJson = itemsArray.find(function(i){
+//                     return i.id == item.id
+//                 })
+//                 total = total + itemJson.price * item.quantity
+//                 console.log(total)
+//             })
+//             stripe.charges.create({
+//                 amount: total,
+//                 source: req.body.stripeTokenId,
+//                 currency: 'usd'
+//             }).then(function(){
+//                 console.log('Charge Successful')
+//                 res.json({ message: 'Successfully purchased items'})
+//             }).catch(function(){
+//                 console.log('Charge Fail')
+//                 res.status(500).end()
+//             })
 
-        }
-    })
-})
+//         }
+//     })
+// })
 
-app.post('/purchase', function(req, res) {
-    fs.readFile('items.json', function(error, data){
-        if(error) {
-            res.status(500).end()
-        } else {
-            console.log('purchase', data)
-            const itemsJson = JSON.parse(data)
-            const itemsArray = itemsJson.music.concat(itemsJson.merch)
-            let total = 0
-            req.body.items.forEach(function(item) {
-                const itemJson = itemsArray.find(function(i){
-                    return i.id == item.id
-                })
-                total = total + itemJson.price * item.quantity
-                console.log(total)
-            })
-            stripe.charges.create({
-                amount: total,
-                source: req.body.stripeTokenId,
-                currency: 'usd'
-            }).then(function(){
-                console.log('Charge Successful')
-                res.json({ message: 'Successfully purchased items'})
-            }).catch(function(){
-                console.log('Charge Fail')
-                res.status(500).end()
-            })
+// app.post('/purchase', function(req, res) {
+//     fs.readFile('items.json', function(error, data){
+//         if(error) {
+//             res.status(500).end()
+//         } else {
+//             console.log('purchase', data)
+//             const itemsJson = JSON.parse(data)
+//             const itemsArray = itemsJson.music.concat(itemsJson.merch)
+//             let total = 0
+//             req.body.items.forEach(function(item) {
+//                 const itemJson = itemsArray.find(function(i){
+//                     return i.id == item.id
+//                 })
+//                 total = total + itemJson.price * item.quantity
+//                 console.log(total)
+//             })
+//             stripe.charges.create({
+//                 amount: total,
+//                 source: req.body.stripeTokenId,
+//                 currency: 'usd'
+//             }).then(function(){
+//                 console.log('Charge Successful')
+//                 res.json({ message: 'Successfully purchased items'})
+//             }).catch(function(){
+//                 console.log('Charge Fail')
+//                 res.status(500).end()
+//             })
 
-        }
-    })
-})
+//         }
+//     })
+// })
 
 
 app.listen(3000)
